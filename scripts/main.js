780fce167258fc275b778e66caac9f9143d66a91
theParadox42 <nathaniel.fargo@gmail.com>
2020-05-19 15:27:51 -0600

    Made it so you can move

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,13 @@
 new p5();
 let cam;
-let mouseSensitivity = 0.005; //change this variable to change the sensitivity if i haven't added a settings menu yet...
+// since we are working in DEGREES now this is much higher
+let mouseSensitivity = 1; //change this variable to change the sensitivity if i haven't added a settings menu yet...
 function setup(){
     let canvas = createCanvas(windowWidth, windowHeight, WEBGL);
     pointerLockSetup();
     cam = createCamera();
     setCamera(cam);
+    angleMode(DEGREES);
 }
 
 let keys = {}, clicked;
